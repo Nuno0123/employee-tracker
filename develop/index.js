2fa47583a0353944c582514b0c1454c41338480a
Nestor <you@example.com>
2023-06-14 17:22:10 -0400

    created function addEmployee

--- develop/index.js
+++ develop/index.js
@@ -85,6 +85,61 @@ function viewAllEmployees() {
       console.table(data);
       startQuestions();
     });
-  }
+}
+
+function addEmployee() {
+    prompt([
+      { name: "firstName", message: "What is the employee`s first name?" },
+      { name: "lastName", message: "What is the employee`s last name?" },
+    ]).then(({firstName, lastName}) => {
+      const roleData =connection.promise().query('SELECT * FROM role')
+      roleData.then(([data]) => {
+          const roleChoices = data.map(({id, title }) => ({
+              name: title,
+              value: id
+          }))
+  
+          
+          prompt({
+              type: 'list',
+              name: 'selectedRole',
+              message:'What is their role?',
+              choices: roleChoices
+          }).then(res => {
+             const roleId = res.selectedRole;
+          const managerData = connection.promise().query('SELECT * FROM employee');
+          managerData.then(([data]) => {
+            const managerChoices = data.map(({id, first_name, last_name}) => ({
+              name: `${first_name} ${last_name}`,
+              value: id,
+            }));
+            managerChoices.unshift({name: 'None', value: null});
+  
+            prompt({
+              type: 'list',
+              name: 'selectedManager',
+              message: 'Who is their manager? (you must select one)',
+              choices: managerChoices,
+            }).then(managerRes => {
+              const managerId = managerRes.selectedManager;
+              const query = connection.promise().query(
+                'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)',
+                [firstName, lastName, roleId, managerId],
+              );
+              query.then(() => {
+                console.log('Employee entered successfully.');
+                startQuestions();
+              });
+            });
+          });
+        });
+      });
+    });
+}
+
+
+  
+
+
 
 
